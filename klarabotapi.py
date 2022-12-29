3b53bc502dfc99f94bd31fdfa38e1d0ecbed821b
Inkogniko <nikola.stanic@students.bfh.ch>
2022-12-29 17:25:53 +0100

    CORS Compatibilty added and tested.

--- klarabotapi.py
+++ klarabotapi.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, MethodView
-from chatterbot import ChatBot
-import json, ast, time
+from flask_restful import Api, MethodView
+from flask_cors import CORS, cross_origin
+import time
 from klarabot import *
 
 #Initialize Flask API
 app = Flask(__name__)
 api = Api(app)
+
+CORS(app)
 
 ##Makes the bot work for some reason
 ##https://stackoverflow.com/a/69802850
