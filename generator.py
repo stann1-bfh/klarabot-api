c6b3fe2110d1796ffb4955b311529534eeee93c7
Inkogniko <nikola.stanic@students.bfh.ch>
2023-01-07 20:32:42 +0100

    Refactor, comment and Creation of Training Data

--- generator.py
+++ generator.py
@@ -1,10 +1,11 @@
 import yaml
+import sys
 import nlpaug.augmenter.word as naw
 from textblob import TextBlob
+from tqdm import tqdm
 
 # Extract the conversation strings from the YAML DataStructure
 def extractConversations(data):
-    
     conversations = []
     for conversation in data['conversations']:
         conversations.append(conversation)
@@ -16,9 +17,9 @@ def saveToYAMLFile(dataStructure):
         yaml.dump(dataStructure, outfile, default_flow_style=False, allow_unicode=True)
 
 # Create variations of a given sentence
-def createVariations(sentence: str, iterations: int):
+def createVariations(sentence: str, randomness: int, iterations: int):
     # 10% Chance to create a Synonym of a word
-    syn_aug = naw.SynonymAug(aug_p=0.1)
+    syn_aug = naw.SynonymAug(aug_p=randomness)
     return syn_aug.augment(sentence, n=iterations)
 
 # Translate the string from German to English
@@ -35,27 +36,39 @@ def openDataset(file_path: str):
     with open(file_path, encoding='utf-8') as f:
         return yaml.safe_load(f)
 
+# Create Variations of Conversations in a YAML-Dataset
+def dataAugmentation(completeDataSet: list):
+    newConv = []
+    counter = 1
+    for set in tqdm(completeDataSet, desc="All Datasets", position=0, colour='#BE88FF'):
+        #Setup the Progressbar
+        currentCount = "Dataset " + str(counter)
+        # Translate German Sentences to be able to create variations
+        initialQuestion = toEnglish(set[0])
+        answerToBeMappedTo = toEnglish(set[1])
+        # Create Variations using the translated sentences
+        variations = createVariations(initialQuestion, variationChange, numberOfVariations)
+        for variation in tqdm(variations, desc=currentCount, colour='green'):
+            newSet = []
+            newSet.append(toGerman(variation))
+            newSet.append(toGerman(answerToBeMappedTo))
+            newConv.append(newSet)
+        counter = counter + 1
+    return newConv
+
 if __name__ == '__main__':
-    file_name = "training_data_papr001.yaml"
+    # Setup Constants
+    file_name = str(sys.argv[1])
+    variationChange = float(sys.argv[2])
+    numberOfVariations = int(sys.argv[3])
     file_path = "training_data/" + file_name
     dataset = openDataset(file_path)
     data = extractConversations(dataset)
-    saveToYAMLFile(dataset)
 
-    # TODO Refactor the variation Loop
-    newConv = []
-    for set in data:
-        initialQuestion = toEnglish(set[0])
-        answerToBeMappedTo = toEnglish(set[1])
-        variations = createVariations(initialQuestion, 25)
-        for variation in variations:
-            newSet = []
-            newSet.append(answerToBeMappedTo)
-            newSet.append(toGerman(variation))
-            newConv.append(newSet)
-
+    # Add Processed Data to new dictionary to be saved
     newDataSet = dict(
-        categories=dataset.get('categories'),
-        conversations=newConv
+        categories = dataset.get('categories'),
+        conversations = dataAugmentation(data)
     )
+    # Save new processed Dataset
     saveToYAMLFile(newDataSet)
--- klarabotapi.py
+++ klarabotapi.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
 from flask_restful import Api, MethodView
-from flask_cors import CORS, cross_origin
-import time
+from flask_cors import CORS
 from klarabot import *
+import time
 
 #Initialize Flask API
 app = Flask(__name__)
 api = Api(app)
-
+#Enable Cross Origin Resource Sharing
 CORS(app)
 
 ##Makes the bot work for some reason
+##TODO: Try to remove this.
 ##https://stackoverflow.com/a/69802850
 time.clock = time.time
 initChatBot("Klarabot")
@@ -23,14 +24,14 @@ class KlarabotAPI(MethodView):
         data = {'message': str(response), 'confidence': response.confidence}
         return (data), 200
 
-##Config API Paths
-api.add_resource(KlarabotAPI, '/klarabotapi')
-
 ##Config Default Path to Index-Website
 @app.route('/')
 def index():
     #Define welcoming Page
     return "<h1>Welcome to the Klarabot-API</h1><p>This is a work in progress.</p>"
+
+##Config API Paths
+api.add_resource(KlarabotAPI, '/klarabotapi')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
